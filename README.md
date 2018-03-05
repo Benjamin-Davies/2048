4660fdcc094773b6e84ad09687d2fca95325fb91
Benjamin Davies <bentendavies@gmail.com>
2018-03-05 17:35:53 +1300

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# 2048 (Clone)
-
-A clone of the game [2048](https://git.io/2048). I made this purely to advance my skills. You can find a live demo at https://benjamin-davies.github.io/2048/.
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import 'materialize-css/dist/css/materialize.min.css';
-import * as React from 'react';
-import { render } from 'react-dom';
-
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-render(<App />, document.getElementById('root'));
-registerServiceWorker();
