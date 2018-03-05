228b20c755674c0a34a1ce1d769a7c3b17acb381
Benjamin Davies <bentendavies@gmail.com>
2018-03-06 07:47:55 +1300

    Add code to resize grid for small screen sizes

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,10 @@ import * as React from 'react';
 import { render } from 'react-dom';
 
 import App from './App';
+
+import addEventListeners from './addEventListeners';
 import registerServiceWorker from './registerServiceWorker';
 
 render(<App />, document.getElementById('root'));
 registerServiceWorker();
+addEventListeners();
