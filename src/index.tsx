f683f91855f4c9d31d19386c359f7f85628b204a
Benjamin Davies <bentendavies@gmail.com>
2018-03-06 16:28:49 +1300

    Undo really bad way of making page resize

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import { render } from 'react-dom';
 
 import App from './App';
 
-import addEventListeners from './addEventListeners';
 import registerServiceWorker from './registerServiceWorker';
 
 render(<App />, document.getElementById('root'));
 registerServiceWorker();
-addEventListeners();
