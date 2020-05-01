54abe176f26bdbc3f1ea1cfa72acfefad9116a39
Benjamin-Davies <bentendavies@gmail.com>
2020-05-02 11:51:12 +1200

    Updates

---
+++ precache-manifest.c9b0aedf1dd8cfb29b8496c0d4fcba1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39de9cadc5714e82955682d3d6b6c7e",
+    "url": "/2048/index.html"
+  },
+  {
+    "revision": "df2deac2c65b66738225",
+    "url": "/2048/static/css/2.658eb2da.chunk.css"
+  },
+  {
+    "revision": "b053357f72bfe5c085ac",
+    "url": "/2048/static/css/main.1187af23.chunk.css"
+  },
+  {
+    "revision": "df2deac2c65b66738225",
+    "url": "/2048/static/js/2.14e90a4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2048/static/js/2.14e90a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b053357f72bfe5c085ac",
+    "url": "/2048/static/js/main.c8ec4b0d.chunk.js"
+  },
+  {
+    "revision": "da4495ad05138c2b317f",
+    "url": "/2048/static/js/runtime-main.f77eab94.js"
+  }
+]);
